469df8bab8cb8a26e1f272e20dd21128ffdfa3c7
AbdiAhmed6457 <samuelahmed6457@gmail.com>
2025-03-12 11:14:54 -0700

    working on header and footer

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,18 @@
+
+function Header(){
+
+    return(
+        <header>
+            <h1> My website</h1>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">services</a></li>
+                <li><a href="#">contact</a></li>
+                <li><a href="#">about</a></li>
+            </ul>
+            <hr />
+        </header>
+    )
+}
+
+export default Header;
