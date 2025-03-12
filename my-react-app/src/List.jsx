2dd1c33bda5a204450ae0d20d0fd0b6be3d3ee78
AbdiAhmed6457 <samuelahmed6457@gmail.com>
2025-03-12 16:41:58 -0700

    done the list default

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,22 +1,18 @@
+import './List.css';
 
+function List({ items, catagory = "catagory" }) {
+  const listItems = items.map((item) => (
+    <li key={item.id}>
+      {item.name}: &nbsp; <b>{item.calories}</b>
+    </li>
+  ));
 
-
-function List(props){
-
-   const itemList = props.items;
-
-    const listItems = itemList.map(item => <li key={item.id}>
-        {item.name}: &nbsp; <b>
-        { item.calories}
-            </b></li>)
-
-
-
-    return ( <>
-      <h3>{props.catagory}</h3>
-          <ul>{listItems}</ul>
+  return (
+    <>
+      <h3 className="list-catag">{catagory}</h3>
+      <ul className="item-list">{listItems}</ul>
     </>
-    )
-} 
+  );
+}
 
 export default List;
